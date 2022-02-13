3b92bb6d4d6d1270ba8e334fb9a3c522d4b0b5a1
naokilh <821874555@qq.com>
2022-02-13 15:31:15 +0800

    update

--- lc1189.go
+++ lc1189.go
@@ -1,5 +1,30 @@
 package main
 
 func maxNumberOfBalloons(text string) int {
+	b, a, l, o, n := 0, 0, 0, 0, 0
 
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		} else {
+			return b
+		}
+	}
+
+	for _, v := range text {
+		switch v {
+		case 'a':
+			a++
+		case 'b':
+			b++
+		case 'l':
+			l++
+		case 'o':
+			o++
+		case 'n':
+			n++
+		}
+	}
+
+	return min(n, min(min(a, b), min(l/2, o/2)))
 }
